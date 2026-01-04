dc9471826a1aa0e5f63216c33632ca64cb8c23b5
Kuroissaint <kurobasaint04@gmail.com>
2026-01-04 14:55:44 +0700

    Delete backend directory

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/src/routes/authRoutes.js
-const AuthController = require('../controllers/AuthController');
-
-async function authRoutes(fastify, options) {
-    fastify.post('/login', AuthController.login);
-    fastify.post('/register', AuthController.register);
-}
-
-module.exports = authRoutes;
