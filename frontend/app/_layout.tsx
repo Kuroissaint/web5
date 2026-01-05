bd6c5178d45830877b2f42cb11ad01dd5bc17ef1
Kuroissaint <kurobasaint04@gmail.com>
2026-01-05 20:56:28 +0700

    Edit model dikit

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> 
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
+      <Stack.Screen name="my-search" options={{ headerShown: false }} />
       
       <Stack.Screen 
         name="searchReport" 
